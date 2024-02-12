2e02399b3b4271eaa608a8ff8afc715d69a9fa75
sarah7 <sarah7_2000@naver.com>
2024-02-12 12:06:05 +0900

    20240212

--- c++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ c++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,87 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <string>
+#include <vector>
 
-#include <iostream>
+#include <chrono>
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+std::vector<char> vBuffer(20 * 1024);
+
+void GrabSomeData(asio::ip::tcp::socket& socket)
+{
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](std::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				std::cout << "\n\nRead " << length << " bytes\n\n";
+
+				for (int i = 0; i < length; i++)
+					std::cout << vBuffer[i];
+
+				GrabSomeData(socket);
+			}
+		}
+	);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	asio::error_code ec;
+
+	asio::io_context context;
+
+	asio::io_context::work idlWork(context);
+
+	std::thread thrContext = std::thread([&]() { context.run(); });
+
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49"	, ec), 80);
+
+	asio::ip::tcp::socket socket(context);
+
+	socket.connect(endpoint, ec);
+
+	if (!ec)
+	{
+		std::cout << "Connected!" << std::endl;
+	} else
+	{
+		std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
+	}
+
+	if (socket.is_open())
+	{
+		GrabSomeData(socket);
+
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+				
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		//using namespace std::chrono_literals;
+		//std::this_thread::sleep_for(2000ms);
+
+		//context.stop();
+		if (thrContext.joinable()) 
+			thrContext.join();
+		
+		/*socket.wait(socket.wait_read);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes Available: " << bytes << std::endl;
+
+		if (bytes > 0)
+		{			
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto c : vBuffer)
+				std::cout << c;
+		}*/
+	}
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
